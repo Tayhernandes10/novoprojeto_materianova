48286612ce3e4fb749ec598fb10169df8cf1a603
TA�ARA FERNANDA SANCHES DIAS <tayara.5970@aluno.pr.senac.br>
2023-09-06 16:34:50 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/novoprojeto/Novoprojeto.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.novoprojeto;
+
+/**
+ *
+ * @author tayara.5970
+ */
+public class Novoprojeto {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+      
+        numeros [0] = 12;
+        numeros [1] = 50;
+        numeros [2] = 78;
+        numeros [3] = 140;
+        numeros [4] = 6;
+        
+        int soma = 0;
+        for(int i = 0; i < 5; i++){
+        soma = soma + numeros[i];
+   
+        }
+        System.out.println("soma");
+    }
+    
+}
